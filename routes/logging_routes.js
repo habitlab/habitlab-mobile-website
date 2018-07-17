ccb59f53e0eba8766c57e1f9bf2074dc0ff7bc93
DrewGregory <djgregny@gmail.com>
2018-07-17 14:33:52 -0700

    generaladdtolog

--- routes/logging_routes.js
+++ routes/logging_routes.js
@@ -65,7 +65,8 @@ app.post('/helloworld', async function(ctx) {
 app.get('/addtolog', async function(ctx) {
   ctx.type = 'json'
   let data = ctx.request.query
-  const {userid, logname} = data
+  const {userid} = data
+  let logname= "" // Each user just has one collection, since our log object has everything for each day
   try {
     var [collection,db] = await get_collection_for_user_and_logname(userid, logname)
     data.timestamp = Date.now()
@@ -320,42 +321,5 @@ sum_time_of_period = function(moment_obj, period, object) {
   }
   return total_time    
 }
-
-/*
-app.get '/addtolog', (ctx) ->>
-  ctx.type = 'json'
-  {userid, logname} = ctx.request.query
-  ctx.body = JSON.stringify {response: 'you gave the followign data', data: ctx.request.query}
-
-  return
-  # {itemid} = ctx.request.body
-  logname = logname.split('/').join(':')
-  if not userid?
-    ctx.body = JSON.stringify {response: 'error', error: 'need parameter userid'}
-    return
-  if not logname?
-    ctx.body = JSON.stringify {response: 'error', error: 'need parameter logname'}
-    return
-  #if not itemid?
-  #  ctx.body = JSON.stringify {response: 'error', error: 'need parameter itemid'}
-  #  return
-  #if itemid.length != 24
-  #  ctx.body = JSON.stringify {response: 'error', error: 'itemid length needs to be 24'}
-  #  return
-  try
-    [collection,db] = await get_collection_for_user_and_logname(userid, logname)
-    #ctx.request.body._id = mongodb.ObjectId.createFromHexString(itemid)
-    if ctx.request.body.timestamp?
-      ctx.request.body.timestamp_local = ctx.request.body.timestamp
-    ctx.request.body.timestamp = Date.now()
-    await n2p -> collection.insert(fix_object(ctx.request.body), it)
-  catch err
-    console.error 'error in addtolog'
-    console.error err
-  finally
-    db?close()
-  #ctx.body = JSON.stringify {response: 'error', error: 'not yet implemented'}
-  ctx.body = JSON.stringify {response: 'success', success: true}
-*/
 
 require('libs/globals').add_globals(module.exports)
